6bca8f865111370f5011d5f2cc480e7f1ff67c23
Nipunn Koorapati <nipunn@dropbox.com>
2016-01-29 22:09:23 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "unittest2pytest",
+    version = "1.0",
+    description='Convert unittest asserts to pytest rewritten asserts',
+    keywords='stopwatch dropbox',
+    license='Apache License 2.0',
+    author='Nipunn Koorapati',
+    author_email='nipunn@dropbox.com',
+    py_modules=['unittest2pytest'],
+    url='https://github.com/dropbox/unittest2pytest',
+
+    install_requires = [],
+)
